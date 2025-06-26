ded1eab39564a5ad148952bdcfd742b950415aa6
Henry Pendleton <hhpendleton@gmail.com>
2025-06-25 22:33:12 -0400

    feat: Add AppleIcon component and replace inline SVG in Home page

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -255,6 +255,7 @@ function HPLogoIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
 function HPLogoIconWhite(
   props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
 ) {
+  // Fixed React DOM properties
   return (
     <svg
       width="100px"
@@ -279,9 +280,9 @@ function HPLogoIconWhite(
           <text
             id="string"
             transform="matrix(1.0 0.0 0.0 1.0 221.0 279.0)"
-            text-decoration="none"
-            font-family="Times-Roman, Times"
-            font-size="253.355"
+            textDecoration="none"
+            fontFamily="Times-Roman, Times"
+            fontSize="253.355"
             fill="#ffffff"
             y="190.0"
             x="1.0"
@@ -294,18 +295,31 @@ function HPLogoIconWhite(
           <text
             id="string-1"
             transform="matrix(1.0 0.0 0.0 1.0 328.0 279.0)"
-            font-family="Times-Roman, Times"
+            fontFamily="Times-Roman, Times"
             y="190.0"
             x="1.0"
-            font-size="253.355"
+            fontSize="253.355"
             fill="#ffffff"
-            text-decoration="none"
+            textDecoration="none"
           >
             P
           </text>
         </defs>
         <use id="P" xlinkHref="#string-1" />
       </g>
+    </svg>
+  );
+}
+
+function AppleIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
     </svg>
   );
 }
@@ -323,4 +337,5 @@ export {
   GitGraphIcon,
   HPLogoIcon,
   HPLogoIconWhite,
+  AppleIcon,
 };
